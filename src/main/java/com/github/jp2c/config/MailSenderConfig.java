809d2221b1f9c6a1f0fbdfe0cbb8bcdbd4c360ce
MaiT <maitmus@gmail.com>
2025-10-27 17:00:43 +0900

    fix: bean

--- src/main/java/com/github/jp2c/config/MailSenderConfig.java
+++ src/main/java/com/github/jp2c/config/MailSenderConfig.java
@@ -1,6 +1,5 @@
 package com.github.jp2c.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -8,29 +7,10 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 @Configuration
-@Profile({"k8s"})
 public class MailSenderConfig {
-    @Value("${spring.mail.host}")
-    private String host;
-
-    @Value("${spring.mail.port}")
-    private int port;
-
-    @Value("${spring.mail.username}")
-    private String username;
-
-    @Value("${spring.mail.password}")
-    private String password;
-
     @Bean
-    public JavaMailSender getJavaMailSender() {
-        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-
-        mailSender.setHost(host);
-        mailSender.setPort(port);
-        mailSender.setUsername(username);
-        mailSender.setPassword(password);
-
-        return mailSender;
+    @Profile("!k8s")
+    public JavaMailSender getMockJavaMailSender() {
+        return new JavaMailSenderImpl();
     }
 }
