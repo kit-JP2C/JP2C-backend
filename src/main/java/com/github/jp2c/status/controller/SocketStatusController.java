1d99b81412760a3939bb8963a1362b422ef80500
MaiT <maitmus@gmail.com>
2025-10-29 14:08:16 +0900

    fix: socket-status

--- src/main/java/com/github/jp2c/status/controller/SocketStatusController.java
+++ src/main/java/com/github/jp2c/status/controller/SocketStatusController.java
@@ -1,7 +1,6 @@
 package com.github.jp2c.status.controller;
 
 import com.github.jp2c.common.dto.CommonResponse;
-import com.github.jp2c.room.service.RoomService;
 import com.github.jp2c.status.dto.CurrentSocketStatusResponse;
 import com.github.jp2c.status.service.SocketConnectionTracker;
 import io.swagger.v3.oas.annotations.Operation;
@@ -14,12 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/v1/socket-status")
 @RequiredArgsConstructor
 public class SocketStatusController {
-    private final RoomService roomService;
     private final SocketConnectionTracker socketConnectionTracker;
 
     @GetMapping
-    @Operation(summary = "현재 접속중인 클라이언트 수/방 수")
+    @Operation(summary = "현재 접속중인 클라이언트 수")
     public CommonResponse<?> getCurrentSocketStatus() {
-        return new CommonResponse<>(new CurrentSocketStatusResponse(socketConnectionTracker.getActiveClientCount(), roomService.getAllRoomLists().size()));
+        return new CommonResponse<>(new CurrentSocketStatusResponse(socketConnectionTracker.getActiveClientCount()));
     }
 }
--- src/main/java/com/github/jp2c/status/dto/CurrentSocketStatusResponse.java
+++ src/main/java/com/github/jp2c/status/dto/CurrentSocketStatusResponse.java
@@ -9,5 +9,4 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class CurrentSocketStatusResponse {
     private int clients;
-    private int rooms;
 }
